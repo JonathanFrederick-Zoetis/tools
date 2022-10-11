35eca373c7aa49f05cd61b3243a2254d40e8b556
Jonathan Frederick <jonathan.frederick@zoetis.com>
2022-10-11 13:48:43 -0400

    quotes around the array

--- isstiautng.sh
+++ isstiautng.sh
@@ -5,7 +5,7 @@ apt-get autoclean
 # Re-enable some useful settings
 declare -a arrayCommands
 arrayCommands=(
-"gsettings set org.gnome.desktop.wm.keybindings switch-applications ['<Super>Tab', '<Alt>Tab']"
+"gsettings set org.gnome.desktop.wm.keybindings switch-applications \"['<Super>Tab', '<Alt>Tab']\""
 "gsettings set org.gnome.mutter overlay-key 'Super_L'"
 "gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding ''"
 )
